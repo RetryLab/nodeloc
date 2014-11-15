58e860ab5f3e28871b144cec839945ec4e68dc84
Tad Wang <wptad@tom.com>
2014-11-16 00:33:15 +0800

    add public api for qqwry only

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var ipQuery = require('./lib/ipQuery'),
 	indexer = require('./lib/indexer');
 
 exports.getCityInfoByIp = ipQuery.getCityInfoByIp;
+exports.getQQwryInfo = ipQuery.getQQwryInfo;
 exports.getCityInfoByAddress = indexer.getCityInfoByAddress;
 
 if(!module.parent){
-	var info = exports.getCityInfoByIp('118.244.254.200');
+//	var info = exports.getCityInfoByIp('118.244.254.200');
+	var info = exports.getQQwryInfo('122.70.31.13');
 	console.log(info);
 }
